3cb8a215cddd5f631771c6452559ba2a679e6151
Ana Có <ana.cbe14@gmail.com>
2020-03-29 11:52:06 -0300

    knex config

---
+++ backend/src/database/migrations/20200328195841_create_incidents.js
@@ -0,0 +1,20 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable('incidents', function(table) {
+    table.increments(); // Gera o ID de forma automática
+    table.string('title').notNullable();
+    table.string('description').notNullable();
+    table.decimal('value').notNullable();
+
+
+    // Relacionamento (PrimaryKey)
+    table.string('ong_id').notNullable();
+
+    // Relacionamento (ForignKey)
+    table.foreign('ong_id').references('id').inTable('ongs');
+  })
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTable('incidents');
+};
