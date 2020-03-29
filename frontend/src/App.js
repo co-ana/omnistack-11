ad48e8e3e361363b5f7396e7f30e62459982bf32
Ana Có <ana.cbe14@gmail.com>
2020-03-29 18:22:36 -0300

    using states

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Header from './Header';
 
 function App() {
-  let counter = 0;
+  // let counter = useState(0); // useState retorna um array -> [valor, funcaoAtualizacao], entao vou precisar de um array para armazenar isso:
+  const [counter, setCounter] = useState(0);
+
 
   function increment() {
-    counter++;
-    console.log(counter);
+    // counter++; // como transformamos a variavel em array, e agora temos uma funcao para utilizar o estado, vamos chamá-la:
+    setCounter(counter+1); // Não dá pra fazer com o "++" :(
   }
 
   return (
