cb3ebfd23edd64404829da749f6104a55971e748
Ana Có <ana.cbe14@gmail.com>
2020-03-29 16:11:59 -0300

    creating profile controller and route

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,6 +2,7 @@ const express = require('express'); // Pacote utilziado para criar as rotas
 
 const OngController = require('./controllers/OngController');
 const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
 
 const routes = express.Router(); // instancia o pacote
 
@@ -11,5 +12,7 @@ routes.post('/ongs', OngController.create);
 routes.get('/incidents', IncidentController.index);
 routes.post('/incidents', IncidentController.create);
 routes.delete('/incidents/:id', IncidentController.delete);
+
+routes.get('/profile', ProfileController.index);
 
 module.exports = routes; // Disponibiliza as rotas para importarmos no file ./index.js
