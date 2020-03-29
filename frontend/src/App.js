86d341fe8cd62e18224839c1bc6e12d48942723b
Ana Có <ana.cbe14@gmail.com>
2020-03-29 18:14:42 -0300

    btn increment without state

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 
+import Header from './Header';
 
 function App() {
+  let counter = 0;
+
+  function increment() {
+    counter++;
+    console.log(counter);
+  }
+
   return (
-    <p>
-      Hello, world!
-    </p>
+    <>
+      <Header>Contador: {counter}</Header>
+      <button onClick={increment}>Incrementar</button>
+    </>
   );
 }
 
